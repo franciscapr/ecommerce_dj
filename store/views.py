b16a64eb14257573a2dd0a51b90fedece725c381
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-08 18:35:56 -0300

    search

--- store/views.py
+++ store/views.py
@@ -4,6 +4,8 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
+
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -12,13 +14,13 @@ def store(request, category_slug=None):
     
     if category_slug != None:    # Si category_slug es diferente de none es decir nulo
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=categories, is_available=True).order_by('id')
         paginator = Paginator(products, 6)  # Agrupamos en grupos de 5
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)  # Agrupamos en grupos de 5
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -46,3 +48,17 @@ def product_detail(request, category_slug, product_slug):
     }
     
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:    # Saber si estamos recibiendo desde la url el keyword
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    
+    return render(request, 'store/store.html', context)
