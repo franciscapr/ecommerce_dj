69e45e70640c7432f5dca60dee89d740f12cdfb7
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-09 14:55:06 -0300

    model variant

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')   # Campos que queremos listar en el admin
     prepopulated_fields = {'slug': ('product_name',)}    # Indicamos como autorrellenamos
     
-    # Registramos la entidad
+    
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+    
+    
+    
+# Registramos la entidad
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
