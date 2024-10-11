9b5265513efa8d5dd62c4cf876b9d16f8136f7a1
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-11 18:21:04 -0300

    widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,25 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    # Creamos las cajas de texto que llevara el forms
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingrese Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirmar Password',
+        'class': 'form-control',
+    }))
+    
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']   # Datos para crear un nuevo usuario
         
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Ingrese nombre'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Ingrese apellidos'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Ingrese telefono'
+        self.fields['email'].widget.attrs['placeholder'] = 'Ingrese email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
